029c9563c8bd58920ced62ba6abbe678dcf51702
MMabo <57180306+MMabo@users.noreply.github.com>
2019-12-02 10:39:29 +0000

    Add files via upload

---
+++ OOP_Roulette.py
@@ -0,0 +1,24 @@
+class Outcome:
+    def __init__(self, name, odds):
+        self.name = name
+        self.odds = odds
+
+    def winamount(self, amount):
+        return self.odds*amount
+
+    def __eq__(self, other):
+        if self.name == other.name:
+            return True
+        
+    def __ne__(self,other):
+        if self.name != other.name:
+            return True
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return "{0} ({1}:11)".format(name,odds) # name (odds:1)
+
+    #def __repr__(self):
+        #return "{class_:s}({name!r}, {odds!r})".format(class_=type(self).__name__, **vars(self) )
